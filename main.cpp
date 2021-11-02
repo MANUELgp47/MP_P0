8c97226c93839b2f0a9820db236ba54153f2e42d
MANUELgp47 <62995481+MANUELgp47@users.noreply.github.com>
2021-11-02 20:49:30 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Fecha.h"
+
+using namespace std;
+
+int main()
+{
+
+/*
+///test fecha
+int d, m, a;
+for (int i=0; i<=10; i++){
+
+    cout << "\ndia:"<<endl;
+    cin >> d;
+    cout << "mes:"<<endl;
+    cin >> m;
+    cout << "anio:"<<endl;
+    cin >> a;
+
+    Fecha f(d,m,a);
+    f.ver();
+
+}
+*/
+
+
+    return 0;
+}
